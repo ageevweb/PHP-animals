3877be6d2d9f3ff1964bd8ed2c08f5297ab4254f
ageev andrey <ageevweb@gmail.com>
2020-05-05 11:17:12 +0300

    2l

---
+++ 2/data.php
@@ -0,0 +1,12 @@
+<?php
+  // var_dump($_GET);
+
+  // print_r($_GET);
+
+  $a1 = $_GET['a1'];
+  $a2 = $_GET['a2'];
+
+
+  echo strlen($a1);
+?>
+
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo 'hello world';
-    $a = 6;
-    var_dump($a);
-?>

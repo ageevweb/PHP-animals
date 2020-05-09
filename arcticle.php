37628a36c75e2845ddc476b814614a4634f5e154
ageev andrey <ageevweb@gmail.com>
2020-05-09 16:20:12 +0300

    article style

--- arcticle.php
+++ arcticle.php
@@ -1,30 +1,23 @@
 <?php
-require_once 'core/config.php';
-require_once 'core/function.php';
-$conn = connect();
-$data = selectArticle($conn);
-$tag = getArticleTags($conn);
-close($conn);
+    require_once('./templates/header.php');
+    $data = selectArticle($conn);
+    $tag = getArticleTags($conn);
+    close($conn);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<?php
-$out = '';
-$out .="<h1>{$data['title']}</h1>";
-$out .="<img src='/images/{$data['image']}'>";
-$out .="<div>{$data['description']}</div>";
- echo $out;
-//var_dump($tag);
+<div class="container">
+    <?php
+        $out = '';
+        $out .="<h1>{$data['title']}</h1>";
+        $out .="<img src='/images/{$data['image']}'>";
+        $out .="<div>{$data['description']}</div>";
+        echo $out;
 
-echo '<hr>';
-for ($i=0; $i < count($tag); $i++){
-    echo "<a href='/tag.php?tag={$tag[$i]['tag']}' style='padding: 5px;'>{$tag[$i]['tag']}</a>";
-}
+        echo '<hr>';
+        for ($i=0; $i < count($tag); $i++){
+            echo "<a class='badge badge-info p-2 m-1' href='/tag.php?tag={$tag[$i]['tag']}' style='padding: 5px;'>{$tag[$i]['tag']}</a>";
+        }
+    ?>
+</div>
+<?php
+  require_once('./templates/footer.php');
 ?>
